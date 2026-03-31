939757874157b6692fe551add9185e79f5fe7fda
Bhaskar <bollambhaskar22@gmail.com>
2026-03-31 11:04:30 +0530

    added

---
+++ Index.js
@@ -0,0 +1,27 @@
+require("dotenv").config()
+
+var express = require("express")
+const connectToDatabase = require("./Database/db.js")
+var useRoutes = require("./Routes/userRoutes")
+var productRoutes = require("./Routes/ProductRoutes.js")
+
+var app = express()
+
+
+app.use(express.json())
+
+app.use("/",useRoutes)
+
+app.use("/",productRoutes)
+
+
+
+
+connectToDatabase()
+
+
+var port = process.env.PORT
+
+app.listen(port,()=>{
+    console.log("The server is running");
+})
